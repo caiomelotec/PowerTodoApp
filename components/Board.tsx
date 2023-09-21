8d5fb128ba058fd580e1c23132d939e3a483d1dc
caiomelotec <caio.melotec@icloud.com>
2023-09-22 00:32:23 +0200

    print board on console

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,13 +1,26 @@
 "use client";
 
+import { useBoardStore } from "@/store/BoardStore";
+import { useEffect } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 export const Board = () => {
+  const [board, getBoard] = useBoardStore((state) => [
+    state.board,
+    state.getBoard,
+  ]);
+  useEffect(() => {
+    getBoard();
+  }, [getBoard]);
+
+  console.log(board);
+
   return (
-    <DragDropContext>
-      <Droppable droppableId="board" direction="horizontal" type="column">
-        {(provided) => <div>{/* Rendering all the columns */}</div>}
-      </Droppable>
-    </DragDropContext>
+    <h1>Hello</h1>
+    // <DragDropContext>
+    //   <Droppable droppableId="board" direction="horizontal" type="column">
+    //     {(provided) => <div>{/* Rendering all the columns */}</div>}
+    //   </Droppable>
+    // </DragDropContext>
   );
 };
