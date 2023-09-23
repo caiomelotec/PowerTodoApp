9b5945b013541c7343d70dab05a817997894c4cf
caiomelotec <caio.melotec@icloud.com>
2023-09-23 11:24:51 +0200

    Group todos by status and populate columns

--- lib/getTodosGroupedByColumn.ts
+++ lib/getTodosGroupedByColumn.ts
@@ -1,56 +1,56 @@
 import { databases } from "@/appwrite";
 
 export const getTodosGroupedByColumn = async () => {
-  const data = await databases.listDocuments(
-    process.env.NEXT_PUBLIC_DATABASE_ID!,
-    process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!
-  );
+  try {
+    const data = await databases.listDocuments(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!
+    );
 
-  const todos = data.documents;
+    const todos = data.documents;
 
-  const columns = todos.reduce((acc, todo) => {
-    if (!acc.get(todo.status)) {
-      acc.set(todo.status, {
-        id: todo.status,
-        todos: [],
-      });
-    }
+    // Initialize columns Map with all possible column types
+    const columnTypes: TypedColumn[] = ["todo", "inprogress", "done"];
+    const columns = new Map<TypedColumn, Column>(
+      columnTypes.map((columnType) => [
+        columnType,
+        {
+          id: columnType,
+          todos: [],
+        },
+      ])
+    );
 
-    acc.get(todo.status)!.todos.push({
-      $id: todo.$id,
-      $createdAt: todo.$createdAt,
-      title: todo.title,
-      status: todo.status,
-      // get the image if it exists on the todo
-      ...(todo.image && { image: JSON.parse(todo.image) }),
+    // Group todos by status and populate columns
+    todos.forEach((todo) => {
+      if (columns.has(todo.status)) {
+        const column = columns.get(todo.status)!;
+
+        column.todos.push({
+          $id: todo.$id,
+          $createdAt: todo.$createdAt,
+          title: todo.title,
+          status: todo.status,
+          ...(todo.image && { image: JSON.parse(todo.image) }),
+        });
+      }
     });
 
-    return acc;
-  }, new Map<TypedColumn, Column>());
+    // Sort columns by columnTypes
+    const sortedColumns = new Map(
+      Array.from(columns.entries()).sort(
+        (a, b) => columnTypes.indexOf(a[0]) - columnTypes.indexOf(b[0])
+      )
+    );
 
-  // if columns doesent have inprogress, todo or done, add them with empty
+    const board: Board = {
+      columns: sortedColumns,
+    };
 
-  const columnTypes: TypedColumn[] = ["todo", "inprogress", "done"];
-
-  for (const columnType of columnTypes) {
-    if (columns.get(columnType)) {
-      columns.set(columnType, {
-        id: columnType,
-        todos: [],
-      });
-    }
+    return board;
+  } catch (error) {
+    // Handle errors here, e.g., log the error or return an error object
+    console.error("Error fetching and processing data:", error);
+    throw error;
   }
-
-  // sort columns by columnTypes
-  const sortedColumns = new Map(
-    Array.from(columns.entries()).sort(
-      (a, b) => columnTypes.indexOf(a[0]) - columnTypes.indexOf(b[0])
-    )
-  );
-
-  const board: Board = {
-    columns: sortedColumns,
-  };
-
-  return board;
 };
