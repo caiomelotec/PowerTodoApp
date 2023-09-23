fd05d894d91920876ef713b5314c5d986f9cb534
caiomelotec <caio.melotec@icloud.com>
2023-09-23 11:23:46 +0200

    todocard pt/1

---
+++ components/TodoCard.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import {
+  DraggableProvidedDragHandleProps,
+  DraggableProvidedDraggableProps,
+} from "react-beautiful-dnd";
+
+type Props = {
+  todo: Todo;
+  index: number;
+  id: TypedColumn;
+  innerRef: (element: HTMLElement | null) => void;
+  draggableProps: DraggableProvidedDraggableProps;
+  dragHandleProps: DraggableProvidedDragHandleProps | null | undefined;
+};
+
+export const TodoCard = ({
+  todo,
+  index,
+  id,
+  innerRef,
+  draggableProps,
+  dragHandleProps,
+}: Props) => {
+  return (
+    <div {...draggableProps} {...dragHandleProps} ref={innerRef}>
+      <h1>Hello</h1>
+    </div>
+  );
+};
