41ff50bc96dc7d812a63cb396235eab85b95acde
caiomelotec <caio.melotec@icloud.com>
2023-09-24 20:16:38 +0200

    handledragend / pt1

--- components/Board.tsx
+++ components/Board.tsx
@@ -11,16 +11,52 @@ import {
 import { Column } from "./Column";
 
 export const Board = () => {
-  const [board, getBoard] = useBoardStore((state) => [
+  const [board, getBoard, setBoardState] = useBoardStore((state) => [
     state.board,
     state.getBoard,
+    state.setBoardState,
   ]);
   useEffect(() => {
     getBoard();
   }, [getBoard]);
 
   console.log(board);
-  const handleOnDragEnd = (result: DropResult) => {};
+  const handleOnDragEnd = (result: DropResult) => {
+    const { destination, source, type } = result;
+    // Check if user dragged card outside of board
+    if (!destination) return;
+
+    // Handle column drag
+    // if (type === "column") {
+    //   const entries = Array.from(board.columns.entries());
+    //   const [removed] = entries.splice(source.index, 1);
+    //   entries.splice(destination.index, 0, removed);
+    //   const rearrangedColumns = new Map(entries);
+    //   setBoardState({
+    //     ...board,
+    //     columns: rearrangedColumns,
+    //   });
+    // }
+
+    //Handle card drag
+    const columns = Array.from(board.columns);
+    const startColIndex = columns[Number(source.droppableId)];
+    const finishColIndex = columns[Number(destination.droppableId)];
+
+    const startCol: Column = {
+      id: startColIndex[0],
+      todos: startColIndex[1].todos,
+    };
+
+    const finishCol: Column = {
+      id: finishColIndex[0],
+      todos: finishColIndex[1].todos,
+    };
+
+    if (!startCol || !finishCol) return;
+
+    if (source.index === destination.index && startCol === finishCol) return;
+  };
 
   return (
     <DragDropContext onDragEnd={handleOnDragEnd}>
