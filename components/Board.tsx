1bba09451dde79e4a5b21953e7ba70317b68aba4
caiomelotec <caio.melotec@icloud.com>
2023-09-25 20:23:32 +0200

    handleOnDragEnd

--- components/Board.tsx
+++ components/Board.tsx
@@ -40,6 +40,8 @@ export const Board = () => {
 
     //Handle card drag
     const columns = Array.from(board.columns);
+    // console.log(columns);
+
     const startColIndex = columns[Number(source.droppableId)];
     const finishColIndex = columns[Number(destination.droppableId)];
 
@@ -56,6 +58,44 @@ export const Board = () => {
     if (!startCol || !finishCol) return;
 
     if (source.index === destination.index && startCol === finishCol) return;
+
+    const newTodos = startCol.todos;
+    const [todoMoved] = newTodos.splice(source.index, 1);
+
+    if (startCol.id === finishCol.id) {
+      //same column task drag
+      newTodos.splice(destination.index, 0, todoMoved);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+
+      const newColumns = new Map(board.columns);
+      newColumns.set(startCol.id, newCol);
+
+      setBoardState({ ...board, columns: newColumns });
+    } else {
+      // dragging to another column
+      const finishTodos = Array.from(finishCol.todos);
+      finishTodos.splice(destination.index, 0, todoMoved);
+
+      const newColumns = new Map(board.columns);
+
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+
+      newColumns.set(startCol.id, newCol);
+      newColumns.set(finishCol.id, {
+        id: finishCol.id,
+        todos: finishTodos,
+      });
+
+      // Update in DB
+
+      setBoardState({ ...board, columns: newColumns });
+    }
   };
 
   return (
