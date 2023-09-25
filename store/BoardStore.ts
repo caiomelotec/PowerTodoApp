df3fe7e87638598ad6de4a665138150c674a7e5d
caiomelotec <caio.melotec@icloud.com>
2023-09-25 20:42:32 +0200

    updateTodoInDB

--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,9 +1,11 @@
+import { databases } from "@/appwrite";
 import { getTodosGroupedByColumn } from "@/lib/getTodosGroupedByColumn";
 import { create } from "zustand";
 
 interface BoardState {
   board: Board;
   setBoardState: (board: Board) => void;
+  updateTodoInDB: (todo: Todo, columnId: TypedColumn) => void;
 }
 
 interface BoardActions {
@@ -20,4 +22,16 @@ export const useBoardStore = create<BoardState & BoardActions>((set) => ({
   },
 
   setBoardState: (board) => set({ board }),
+
+  updateTodoInDB: async (todo, columnId) => {
+    await databases.updateDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+      todo.$id,
+      {
+        title: todo.title,
+        status: columnId,
+      }
+    );
+  },
 }));
